bb7f13e586025ee84991b7511fadd82c89ddb579
muhammadbsoul <moh.bsoul.93@gmail.com>
2026-01-12 21:05:41 +0200

    create js file with post and get functions

---
+++ app.js
@@ -0,0 +1,47 @@
+`use strict`
+
+console.log(`api`);
+
+
+const apiKey = `https://696517a8e8ce952ce1f424fd.mockapi.io/Players`;
+
+const startGameBtn = document.querySelector("#btnStartGame");
+const endVoteBtn = document.querySelector("#btnEndVoting");
+const resetBtn = document.querySelector("#btnReset");
+
+const inputPlayerName = document.querySelector("#playerName");
+const postPlayerBtn = document.querySelector("#btnSubmit");
+const cancelEditBtn = document.querySelector("#btnCancel");
+
+async function postPlayer(player) {
+    fetch(apiKey,
+        {
+            method: `POST`,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(player)
+        }).then(getPlayers());
+
+}
+
+async function getPlayers() {
+    const players = [];
+    fetch(apiKey).then(res => res.json()).then(res => console.log(res));
+}
+
+postPlayerBtn.addEventListener(`click`, () => {
+    getPlayers()
+    const name = inputPlayerName.value;
+    if (name.trim() === "") alert(`the name is empty`)
+    else {
+        const player = {
+            name: name,
+            alive: true,
+            vote: 0,
+            isVoted: false,
+            isMafia: false
+        }
+        postPlayer(player);
+        
+    }
+})
+
